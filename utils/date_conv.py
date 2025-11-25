582b4cf0b59942a4ce416f01fc6bc1859dcb93f7
Rowshnick <127508073+Rowshnick@users.noreply.github.com>
2025-11-25 12:46:01 +0330

    Update date_conv.py

--- utils/date_conv.py
+++ utils/date_conv.py
@@ -1,26 +1,9 @@
-# utils/date_conv.py
 from persiantools.jdatetime import JalaliDate
 
 def gregorian_to_jalali(year: int, month: int, day: int) -> dict:
-    """
-    تبدیل تاریخ میلادی به شمسی
-    ورودی: سال، ماه، روز میلادی
-    خروجی: دیکشنری شامل سال، ماه، روز شمسی
-    """
-    try:
-        jd = JalaliDate.from_gregorian(year=year, month=month, day=day)
-        return {"year": jd.year, "month": jd.month, "day": jd.day}
-    except Exception as e:
-        raise ValueError(f"تبدیل میلادی به شمسی ناموفق بود: {e}")
+    jd = JalaliDate.from_gregorian(year=year, month=month, day=day)
+    return {"year": jd.year, "month": jd.month, "day": jd.day}
 
 def jalali_to_gregorian(year: int, month: int, day: int) -> dict:
-    """
-    تبدیل تاریخ شمسی به میلادی
-    ورودی: سال، ماه، روز شمسی
-    خروجی: دیکشنری شامل سال، ماه، روز میلادی
-    """
-    try:
-        gd = JalaliDate(year, month, day).to_gregorian()
-        return {"year": gd.year, "month": gd.month, "day": gd.day}
-    except Exception as e:
-        raise ValueError(f"تبدیل شمسی به میلادی ناموفق بود: {e}")
+    gd = JalaliDate(year, month, day).to_gregorian()
+    return {"year": gd.year, "month": gd.month, "day": gd.day}
